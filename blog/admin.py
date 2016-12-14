de3d554259a38abf8d1d4992fda907ef5f6425e9
karolkujawa <karol.kujawa85@gmail.com>
2016-12-14 23:41:42 +0100

    Dodano komentarze

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import POST
+from .models import POST, Comment
 
 # Register your models here.
 
 admin.site.register(POST)
+admin.site.register(Comment)
--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import POST
+from .models import POST, Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = POST
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
